2850d1bdf36f9075c886f194a31b8bf8ee1e1ba2
limwa <me@limwa.pt>
2020-12-25 02:38:11 +0000

    feat: :sparkles: finish player movement
    This commit introduces a very basic physics engine, where the skier (player) is pulled by gravity and is subject to friction (or air resistance).
    
    When the direction of the skier changes, the previous velocity is projected onto the new direction.
    This creates, accidentally, the intended behaviour of the player not sliding in the ice, even though there is no constraint explicitly enforcing that.
    
    On a final note, the physics engine is most likely not finished and will be changed in the future. Nevertheless, since I am happy with its current state, I'm saying it is finished for now.

--- src/skiing.py
+++ src/skiing.py
@@ -4,9 +4,11 @@
 """
 
 import os
-import sys
 
-from typing import List, Tuple
+import math
+from typing import List, Tuple, Union
+
+from pygame import Vector2
 
 import pygame
 import pygame.locals
@@ -15,18 +17,46 @@ import pygame.image
 import pygame.event
 import pygame.sprite
 import pygame.time
+import pygame.draw
+import pygame.transform
 
+
+# EDIT THESE CONSTANTS AT YOUR WILL
+# SCREEN CONSTANTS
 WIDTH = 800
 HEIGHT = 600
 
-DIFICULTY_MULTIPLIER = 1
-TIME_ADJUST = 0.001 # conversion of ms to s
-SCALE_ADJUST = 100 # convert units to px
+# TIME CONSTANTS
+TIME_ADJUST = 0.001  # conversion of ms to s
+DIFICULTY_MULTIPLIER = 2
+assert DIFICULTY_MULTIPLIER > 0
 
+# ENVIRONMENT CONSTANTS
+G = math.pi ** math.pi
+PLANE_INCLINATION = 60  # inclination of the plane in degrees
+FRICTION_CONSTANT = 0.25  # must be lower than 1
+assert G > 0
+assert 0 < PLANE_INCLINATION < 90
+assert 0 <= FRICTION_CONSTANT <= 30
+
+# CAMERA CONSTANTS
+CAMERA_PADDING = 50
+CAMERA_TRACKING_BOUND = 100
+assert CAMERA_TRACKING_BOUND >= CAMERA_PADDING
+
+
+# DON'T EDIT ANYTHING ELSE
+
+
+TIME_SCALAR = DIFICULTY_MULTIPLIER * TIME_ADJUST
+PLANE_ACCELERATION = G * math.sin(math.radians(PLANE_INCLINATION))
+
+# ASSETS
 ROOT = os.path.join(os.path.dirname(__file__), '..')
 ASSETS = os.path.join(ROOT, 'assets')
 
-def load_image(name):
+
+def load_image(name: str):
     """ Loads an image from the assets folder. """
 
     path = os.path.join(ASSETS, name)
@@ -36,43 +66,91 @@ def load_image(name):
     else:
         img = img.convert_alpha()
 
-    return img
+    return img, img.get_rect()
+###
+
+# GAME LOGIC
+class Camera:
+    def __init__(self, top: float, padding: float):
+        self.top = top
+        self.padding = padding
+        self.offset = 0
+
+    def track(self, pos: Vector2) -> None:
+        self.offset = -pos.y + min(pos.y + self.padding, self.top)
+
+    def transform(self, vector: Union[Vector2, Tuple[float, float], List[float]]) -> Vector2:
+        return Vector2(vector[0], vector[1] + self.offset)
+
 
 class Skier(pygame.sprite.Sprite):
     """ Represents a Skiier (player) in the game """
 
-    CONVERSION_CONSTANT = DIFICULTY_MULTIPLIER * TIME_ADJUST * SCALE_ADJUST
+    @staticmethod
+    def init(states: List[Tuple[float, Tuple[pygame.Surface, pygame.Rect]]]):
+        Skier.__states = states
 
-    def __init__(self, pos: List[float], velocity: List[float], angle: float, inverted: bool) -> None:
+    def __init__(self, pos: Vector2, velocity: Vector2) -> None:
         pygame.sprite.Sprite.__init__(self)
 
         self.pos = pos
-        self.set_velocity(velocity)
-        self.set_angle(0, inverted)
-
-        image = load_image('skier-0.png')
-        self.image = image
-        self.rect = image.get_rect()
-
-    def set_angle(self, angle: float, inverted: bool):
-        self.angle = angle
-        self.inverted = inverted
-
-    def set_position(self, pos: List[float]):
-        self.pos = pos
-
-    def set_velocity(self, velocity: List[float]):
-        assert len(velocity) == len(self.pos), 'Length of velocity vector must be equal to that of position vector'
         self.velocity = velocity
 
-    def update(self, dt):
-        for i in range(len(self.pos)):
-            self.pos[i] += self.velocity[i] * self.CONVERSION_CONSTANT * dt
+        self.__angle = 0
+        self.state = len(self.__states) // 2
 
-        print(f"pos is now {self.pos}")
+    @property
+    def state(self):
+        return self.__state
 
-    def render(self, screen: pygame.Surface) -> None:
-        screen.blit(self.image, self.pos) # type: ignore
+    @state.setter
+    def state(self, s: int):
+        # We need the state to be valid at all times
+        s = max(s, 0)
+        s = min(s, len(self.__states) - 1)
+
+        self.__state = s
+
+        data = self.__states[s]
+
+        proj_length = math.cos(math.radians(
+            data[0] - self.__angle)) * self.velocity.length()
+
+        self.__angle = data[0]
+        self.__angle_rad = math.radians(self.__angle)
+
+        # calculate projection of the velocity on the new direction
+        self.velocity.x = proj_length * math.sin(self.__angle_rad)
+        self.velocity.y = proj_length * math.cos(self.__angle_rad)
+
+        self.image, self.rect = data[1]
+
+    @property
+    def acceleration(self) -> Vector2:
+        return Vector2(
+            0.5 * PLANE_ACCELERATION * math.sin(2 * self.__angle_rad),
+            PLANE_ACCELERATION * math.cos(self.__angle_rad) ** 2
+        ) - FRICTION_CONSTANT * self.velocity  # type: ignore
+
+    def update(self, dt: float) -> None:
+        self.velocity += self.acceleration * dt
+        self.pos += self.velocity * dt
+
+        # We need to cap the horizontal component of the position vector
+        # to the interval [w / 2, WIDTH - w / 2],
+        # where w is the width of the image,
+        # so that the player doesn't fall off the screen
+        if not self.rect.w / 2 <= self.pos.x <= WIDTH - self.rect.w / 2:
+            self.pos.x = max(self.rect.w / 2, self.pos.x)
+            self.pos.x = min(WIDTH - self.rect.w / 2, self.pos.x)
+            self.velocity.x = 0
+
+        print(self.velocity, self.pos)
+
+        self.rect.center = self.pos  # type: ignore
+
+    def render(self, screen: pygame.Surface, camera: Camera) -> None:
+        screen.blit(self.image, camera.transform(self.rect.topleft))  # type: ignore
 
 
 def main():
@@ -86,29 +164,57 @@ def main():
     background = background.convert()
     background.fill((245, 245, 245))
 
-    player = Skier([WIDTH // 2, 50], [0, 1], 0, False)
+    assets = {i[:-4]: load_image(i) for i in os.listdir(ASSETS)}
+
+    def get_img_and_rect(img): return img, img.get_rect()
+    Skier.init([
+        (-90, assets["skier-3"]),
+        (-60, assets["skier-2"]),
+        (-30, assets["skier-1"]),
+        (-15, assets["skier-0"]),
+        (0, assets["skier-0"]),
+        (0, get_img_and_rect(pygame.transform.flip(assets["skier-0"][0], True, False))),
+        (15, get_img_and_rect(pygame.transform.flip(assets["skier-0"][0], True, False))),
+        (30, get_img_and_rect(pygame.transform.flip(assets["skier-1"][0], True, False))),
+        (60, get_img_and_rect(pygame.transform.flip(assets["skier-2"][0], True, False))),
+        (90, get_img_and_rect(pygame.transform.flip(assets["skier-3"][0], True, False)))
+    ])  # type: ignore
+
+    player = Skier(Vector2(WIDTH / 2, 0), Vector2(0, 0))
+    camera = Camera(CAMERA_TRACKING_BOUND, CAMERA_PADDING)
 
     clock = pygame.time.Clock()
+    running = True
     while True:
-        dt = clock.tick(60)
+        dt = clock.tick(60) * TIME_SCALAR
 
         for event in pygame.event.get():
             if event.type == pygame.locals.QUIT:
-                return
+                running = False
+                break
 
-            elif event.type == pygame.locals.KEYDOWN:
-                if event.key == pygame.locals.K_DOWN:
-                    player.set_velocity([0, 1])
-                elif event.key == pygame.locals.K_UP:
-                    player.set_velocity([0, -1])
+            if event.type == pygame.locals.KEYDOWN:
+                if event.key == pygame.locals.K_RIGHT:
+                    player.state += 1
+                elif event.key == pygame.locals.K_LEFT:
+                    player.state -= 1
+
+        if not running:
+            break
 
         player.update(dt)
+        camera.track(player.pos)  # type: ignore
 
         screen.blit(background, (0, 0))
-        player.render(screen) # type: ignore
+        player.render(screen, camera)  # type: ignore
 
+        for i in range(200):
+            pygame.draw.line(screen, (0, 0, 0), camera.transform(
+                (10, i * 100)), camera.transform((WIDTH - 10, i * 100)), 2)
 
         pygame.display.flip()
+###
+
 
 if __name__ == '__main__':
     main()
